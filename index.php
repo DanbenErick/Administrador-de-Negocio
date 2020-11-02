2037704ce7fcc5ab7939ef1d1bc8810f780d1f67
Danben Erick <danbencb08@outlook.es>
2020-11-01 21:38:31 -0500

    Iniciado el archivo principal

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Identifiquese</title>
+</head>
+<body>
+    
+
+    <noscript>Es nesesario tener activado el JS</noscript>
+</body>
+</html>
