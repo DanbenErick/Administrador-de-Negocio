2ab4a5e35dd1e1d6e33bce593b474dad8f626183
Danben Erick <danbencb08@outlook.es>
2020-11-02 22:38:44 -0500

    avance de la pagina de login

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Identifiquese</title>
+    <link rel="stylesheet" href="assets/css/style-index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,300&family=Poppins:ital,wght@0,300;0,400;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Pagina Principal</h1>
+    <section>
+        <div class="container_login">
+            <div class="container_form">
+                <h1>Identifiquese</h1>
+                <form action="">
+                    <div class="input_group">
+                        <label for="usuario">Usuario</label>
+                        <input type="text" id="usuario" name="usuario">
+                    </div>
+                    <div class="input_group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" id="password" name="password">
+                    </div>
+                    <div class="input_group">
+                        <button type="submit">Ingresar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
 
     <noscript>Es nesesario tener activado el JS</noscript>
 </body>
