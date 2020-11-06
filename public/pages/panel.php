1daf073abe76aa92df4fb7c5e795fec5514f4423
Danben Erick <danbencb08@outlook.es>
2020-11-06 12:24:56 -0500

    Terminado el panel y se agrego un snippet con display none

--- public/pages/panel.php
+++ public/pages/panel.php
@@ -10,7 +10,172 @@
     <?php require_once("components/nav.inc.php");?>
     <section>
     <?php require_once("components/aside.inc.php");?>
-        <main></main>
+        <main>
+            <h1 class="titulo">Ultimas Ventas</h1>
+            <div class="container_table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Categoria</th>
+                            <th>Fecha</th>
+                            <th>Empleado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                        <tr>
+                            <td>lapicero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>Jhon</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h2 class="titulo">Ultimas Adquisitiones</h2>
+            <div class="container_table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Categoria</th>
+                            <th>Fecha</th>
+                            <th>Proveedor</th>
+                            <th>Empleado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>CredCorp</td>
+                            <td>Andres</td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>CredCorp</td>
+                            <td>Andres</td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>15</td>
+                            <td>15.00</td>
+                            <td>libreria</td>
+                            <td>12/12/12</td>
+                            <td>CredCorp</td>
+                            <td>Andres</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h2 class="titulo">Ultimas Categorias Agregadas</h2>
+            <div class="container_table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Categoria</th>
+                            <th>Emplado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                        <tr>
+                            <td>lacipero</td>
+                            <td>Eduardo </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <span style="display: none">.container_table>table>(thead>tr>th*2)+(tbody>tr>td*2)</span>
+        </main>
     </section>
 </body>
 </html>
