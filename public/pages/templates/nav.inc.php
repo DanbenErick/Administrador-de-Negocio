0790813162a83c85abbfbf5157bc4fc36f81fa92
DanielBustillosVillar <68455232+DanielBustillosVillar@users.noreply.github.com>
2020-11-17 00:37:59 -0500

    Menu2

--- public/pages/templates/nav.inc.php
+++ public/pages/templates/nav.inc.php
@@ -1,7 +1,8 @@
 <nav>
     <ul class="menu">
-        <li class="logo"><a href="#nav">Almacen</a></li>
+        <li class="logo almacen"><a href="#nav">Almacen</a></li>
         <a href="#nav"><i class="icon-arrow-circle-o-up"></i></a>
+        <ol class="name"><a href="#"><?php echo $_SESSION['nombre']?></a></ol>
         <a href="#"><i class="icon-arrow-circle-o-down"></i></a>
         <li class="logo"><a href="#"><?php echo $_SESSION['nombre']?></a></li>
         
